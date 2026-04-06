107bd9145c7ecb62e7312dd1843f30984140d63a
achuabhi36907-lab <achuabhi36907@gmail.com>
2026-04-06 18:10:09 +0530

    Add files via upload

---
+++ arithmetic operation .c
@@ -0,0 +1,16 @@
+// Abhi ps
+//dbt
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
+
+    printf("Sum = %d\nDifference = %d\nProduct = %d\nQuotient = %.2f",
+    a+b,a-b,a*b,(float)a/b);
+
+    return 0;
+}
